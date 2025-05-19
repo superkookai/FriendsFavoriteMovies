9e85e27bacb21bf9b7f065f791c103198183b2f1
superkookai <weerawut@live.com>
2025-05-19 10:32:33 +0700

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Weerawut Chaiyasomboon on 19/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
